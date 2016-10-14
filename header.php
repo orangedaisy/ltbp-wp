2487f087a903ea36f213064d033de46ec837678b
Robert Showalter <robert@orangedaisy.co>
2016-10-14 16:27:02 -0400

    Add google fonts

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Corben:400,700|Source+Sans+Pro:300,400">
 
 <?php wp_head(); ?>
 </head>
