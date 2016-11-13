63d2183d8fcb8881ae510dff728eb2422c80cc83
Robert Showalter <robert@orangedaisy.co>
2016-11-13 16:03:45 -0500

    Style investments page

--- archive-investments.php
+++ archive-investments.php
@@ -12,17 +12,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <h1>Hello world!</h1>
-
       <?php
         $args = array( 'post_type' => 'investments' );
         $loop = new WP_Query( $args );
         while ( $loop->have_posts() ) : $loop->the_post();
       ?>
 
-        <article class="c-investment">
+        <article class="c-investment-block" style="
+                                      background-image:
+                                        linear-gradient(
+                                          to bottom,
+                                          rgba(34,85,119,.25),
+                                          rgba(34,85,119,.25)
+                                        ),
+                                        url(<?php the_field('investment_image'); ?>);">
 
-          <h1><?php the_field('investment_name'); ?></h1>
+          <div class="o-row g-grid g-grid--columns">
+            <div class="c-investment-block__content">
+              <h1 class="c-investment-block__name"><?php the_field('investment_name'); ?></h1>
+              <div class="c-investment-block__overview"><?php the_field('investment_overview'); ?></div>
+              <p>
+                <a class="c-link c-link--white" href="<?php esc_url( get_permalink() ); ?>" title="<?php the_field('investment_name'); ?>">
+                  Read More
+                </a>
+              </p>
+              <p>
+                <a class="c-button c-button--white" href="/contact" title="Book a Session">Book a Session</a>
+              </p>
+            </div>
+          </div>
 
         </article>
 
