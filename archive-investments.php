24b2a7b1b0e06ac104e75da3dc0ca2c7edc3d80d
Robert Showalter <robert@orangedaisy.co>
2016-11-20 14:49:59 -0500

    Rethink Investments page

--- archive-investments.php
+++ archive-investments.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying investment options.
+ * The template for displaying archive pages.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -12,44 +12,40 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <?php
-        $args = array( 'post_type' => 'investments' );
-        $loop = new WP_Query( $args );
-        while ( $loop->have_posts() ) : $loop->the_post();
-      ?>
+		<?php
+		if ( have_posts() ) : ?>
 
-        <article class="c-investment-block" style="
-                                      background-image:
-                                        linear-gradient(
-                                          to bottom,
-                                          rgba(34,85,119,.6),
-                                          rgba(34,85,119,.6)
-                                        ),
-                                        url(<?php the_field('investment_image'); ?>);">
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-          <div class="o-row g-grid g-grid--columns">
-            <div class="c-investment-block__content">
-              <h1 class="c-investment-block__name"><?php the_field('investment_name'); ?></h1>
-              <div class="c-investment-block__overview">
-                <?php the_field('investment_overview'); ?>
-                <p>
-                  <a class="c-link c-link--white" href="<?php esc_url( get_permalink() ); ?>" title="<?php the_field('investment_name'); ?>">
-                    Read More
-                  </a>
-                </p>
-              </div>
-              <p>
-                <a class="c-button c-button--white" href="/contact" title="Book a Session">Book a Session</a>
-              </p>
-            </div>
-          </div>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-        </article>
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
 
-      <?php endwhile; ?>
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
+//get_sidebar();
 get_footer();
