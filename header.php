0210a05962877c69777fcb28faf7da5496f1c65b
Robert Showalter <robert@orangedaisy.co>
2016-11-21 19:53:36 -0500

    Load Source Sans Pro 400 italic

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Corben:400,700|Source+Sans+Pro:300,400">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Corben:400,700|Source+Sans+Pro:300,400,400i">
 
 <?php wp_head(); ?>
 
